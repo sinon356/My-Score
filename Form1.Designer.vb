a0fe99ac8c9f2050875e9d67d4e6b4301c003d96
Administrator <Administrator@PSPKM2826-2242>
2022-03-28 11:13:56 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label1.Location = New System.Drawing.Point(74, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(236, 118)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Excelent"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 237)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "What is your score?"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim score As Integer
+        Dim ResultAs As String
+
+        score = InputBox("Enter score")
+        If score >= 0 And score <= 49 Then
+            ResultAs = "Failed"
+        ElseIf score <= 74 And score >= 50 Then
+            ResultAs = "Pass"
+        ElseIf score <= 89 And score >= 75 Then
+            ResultAs = "Very Good"
+        ElseIf score >= 90 And score <= 100 Then
+            ResultAs = "Excellent"
+        Else
+            ResultAs = "Please input your score between 0 - 100"
+        End If
+        Label1.Text = ResultAs
+    End Sub
+End Class
